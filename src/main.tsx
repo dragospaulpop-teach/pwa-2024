1647aa363348a44a1dec4a9a89fc3aa7de92f8f8
Dragos-Paul Pop <froind@gmail.com>
2024-10-27 13:41:21 +0200

    view transitions

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ import { routeTree } from "./routeTree.gen";
 // Create a new router instance
 const router = createRouter({
   routeTree,
+  defaultViewTransition: true,
 });
 
 router.subscribe("onLoad", () => {
